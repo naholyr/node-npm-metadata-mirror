055485a1adfcdebce920f791c6c8e58262d8f9ab
Nicolas Chambrier <nchambrier@clever-age.com>
2012-01-14 00:45:14 +0100

    log4js: do not replace console.log

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 var mirror = require('./lib/mirror');
 
+// Expose simplier version of "mirror()" with smart default options
 module.exports = function _mirror (options) { return mirror(fix_options(options)); };
-module.exports.store = mirror.store; // Expose to give access to the predefined store classes
-module.exports.monitor = function _monitor (options) { return mirror.monitor(fix_options(options)); };
-module.exports.log4js = require('log4js'); // Expose to ease logger configuration
 
+// Expose simplier version of "monitor()" with smart default options
+module.exports.monitor = function _monitor (options) { return mirror.monitor(fix_options(options)); };
+
+// Expose the stores
+module.exports.store = mirror.store;
+
+// Expose log4js to ease logger configuration
+module.exports.log4js = require('log4js');
+// Default log4js configuration
+module.exports.log4js.configure({
+  "doNotReplaceConsole": true
+});
+
+// Fill options with smart defaults
 function fix_options (options) {
   options = options || {};
 
