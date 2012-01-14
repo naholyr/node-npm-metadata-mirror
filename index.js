d2b07a8c671be4becc6f3f0943ea9c0dbae4997a
Nicolas Chambrier <nchambrier@clever-age.com>
2012-01-14 02:08:45 +0100

    Fix log4js really don't want us to keep him from overwriting console.log → monkey patch

--- index.js
+++ index.js
@@ -11,10 +11,18 @@ module.exports.store = mirror.store;
 
 // Expose log4js to ease logger configuration
 module.exports.log4js = require('log4js');
-// Default log4js configuration
-module.exports.log4js.configure({
-  "doNotReplaceConsole": true
-});
+// DO NOT FUCKING OVERWRITE CONSOLE.*!! (otherwise, log4js is cool, so let's just fix this)
+(function (log) {
+  // We'd like to simply call restoreConsole()
+  // but we then get "Object #<Object> has no method '_preLog4js_log'" in "node_modules/log4js/lib/appenders/console.js:6:10"
+  // so we have to save it...
+  var l = console._preLog4js_log;
+  // ... restore original console...
+  log.restoreConsole();
+  // ... and finally restore it
+  console._preLog4js_log = l;
+  // and because we touch internals, we specify a hard version in package.json
+})(module.exports.log4js);
 
 // Fill options with smart defaults
 function fix_options (options) {
